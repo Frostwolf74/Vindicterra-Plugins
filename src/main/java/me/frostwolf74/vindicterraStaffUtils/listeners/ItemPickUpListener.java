d72bcab03f1eee3d594b54ddda1cd66b53b88553
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-01-03 19:36:00 -0700

    prevent players who are in staff mode from picking up items

--- src/main/java/me/frostwolf74/vindicterraStaffUtils/listeners/ItemPickUpListener.java
+++ src/main/java/me/frostwolf74/vindicterraStaffUtils/listeners/ItemPickUpListener.java
@@ -2,19 +2,39 @@ package me.frostwolf74.vindicterraStaffUtils.listeners;
 
 import io.papermc.paper.event.player.PlayerPickItemEvent;
 import me.frostwolf74.vindicterraStaffUtils.VindicterraStaffUtils;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.persistence.PersistentDataType;
+import org.checkerframework.checker.units.qual.N;
+
+import javax.naming.Name;
 
 public class ItemPickUpListener implements Listener {
     @EventHandler
     public void onItemPickUpEvent(PlayerAttemptPickupItemEvent e){
-        e.getPlayer().sendMessage("pick up event recorded");
+        e.getPlayer().sendMessage("pick up event recorded: 1");
         if(Boolean.TRUE.equals(e.getPlayer().getPersistentDataContainer().get(new NamespacedKey(VindicterraStaffUtils.getPlugin(), "inStaffMode"), PersistentDataType.BOOLEAN))){
             e.setCancelled(true);
+        }
+    }
 
+    @EventHandler
+    public void onItemPickUpEvent2(PlayerPickItemEvent e){
+        e.getPlayer().sendMessage("pick up event recorded: 2");
+        if(Boolean.TRUE.equals(e.getPlayer().getPersistentDataContainer().get(new NamespacedKey(VindicterraStaffUtils.getPlugin(), "inStaffMode"), PersistentDataType.BOOLEAN))){
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onItemPickUpEvent3(PlayerPickupItemEvent e){
+        e.getPlayer().sendMessage("pick up event recorded: 3");
+        if(Boolean.TRUE.equals(e.getPlayer().getPersistentDataContainer().get(new NamespacedKey(VindicterraStaffUtils.getPlugin(), "inStaffMode"), PersistentDataType.BOOLEAN))){
+            e.setCancelled(true);
         }
     }
 }
